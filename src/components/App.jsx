b22f41c7d7c5f6edaddc7240c38408123d172f68
Tatiana Kashcha <tkashcha@gmail.com>
2023-06-17 18:49:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,10 +10,10 @@ const STORAGE_KEY = 'contact-list';
 class App extends Component {
   state = {
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -54,7 +54,7 @@ class App extends Component {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(el => el.id !== id),
     }));
-    localStorage.removeItem(STORAGE_KEY, JSON.stringify(this.state.contacts));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(this.state.contacts));
   };
 
   handleCangeFilter = e => {
