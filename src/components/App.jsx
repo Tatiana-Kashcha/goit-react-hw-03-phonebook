46f60794b4288f92d282a4ff1a5e4a1ec6cfe95b
Tatiana Kashcha <tkashcha@gmail.com>
2023-06-17 18:32:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,13 +10,21 @@ const STORAGE_KEY = 'contact-list';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      this.setState({
+        contacts: JSON.parse(localStorage.getItem(STORAGE_KEY)),
+      });
+    }
+  }
 
   addUser = data => {
     const newUser = {
@@ -38,12 +46,15 @@ class App extends Component {
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newUser],
     }));
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(this.state.contacts));
   };
 
   deleteUser = id => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(el => el.id !== id),
     }));
+    localStorage.removeItem(STORAGE_KEY, JSON.stringify(this.state.contacts));
   };
 
   handleCangeFilter = e => {
