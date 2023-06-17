15cc2ebd7129ddec898dcf8ecdf146a308febcef
Tatiana Kashcha <tkashcha@gmail.com>
2023-06-17 20:19:42 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,7 @@ const STORAGE_KEY = 'contact-list';
 
 class App extends Component {
   state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -84,10 +79,16 @@ class App extends Component {
       <s.Container>
         <h1>Phonebook</h1>
         <ContactForm addUser={this.addUser} />
-
-        <h2>Contacts</h2>
-        <Filter filter={filter} handleCangeFilter={this.handleCangeFilter} />
-        <ContactList data={searchUser} deleteUser={this.deleteUser} />
+        {this.state.contacts.length > 0 && (
+          <>
+            <h2>Contacts</h2>
+            <Filter
+              filter={filter}
+              handleCangeFilter={this.handleCangeFilter}
+            />
+            <ContactList data={searchUser} deleteUser={this.deleteUser} />
+          </>
+        )}
       </s.Container>
     );
   }
